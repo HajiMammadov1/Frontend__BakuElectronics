6ba587b4b4dab0b34eaa1e7650ce10fbe12cdb7f
Haji Mammadov1 <hajiam@code.edu.az>
2022-03-05 07:07:27 -0800

    add main slider, features section

---
+++ BakuElectronics_Frontend/assets/js/slick.js
@@ -0,0 +1,15 @@
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    prevArrow: '<button type="button" data-role="none" class="slick-prev"><img src="./assets/images/promo-arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next"><img src="./assets/images/promo-arrow-next.svg" alt=""></button>',
+    customPaging:function(slider, i) {
+            return '<button type="button" data-role="none"></button>';
+          },
+    
+        
+  });
